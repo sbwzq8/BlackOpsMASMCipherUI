559a9d61ed84d8a7b1eefd368c93a3594ccbbaf7
sbwzq8 <sbwzq8@mail.missouri.edu>
2019-01-04 04:00:58 -0600

    Working model with simple UI

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BlackOpsMasmCipherSolver
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            StuffToDo();
+
+        }
+        public void StuffToDo()
+        {
+            //set inputs to GK scrap 2 
+            cipherText.Text = "2366981 13 2455638 7833 1969907 17 833 859 0 278775 188055 254 6297303 19 13 51138 1045748 46158 146 3747 349977 13937091 46807 376388 4632 278775 479 3260788 108792 13 57554 191611 407292 13 1774 4632 17028678 51357 833 5086887 167240 185423 1410635 254 4632 10302704 6297303 19 1031361 2470182 14258 13557614 157 4091537 110 4632 1471289 697881 60609 20500575 32 26161 4632 182710 615190 35 76798 8070075 9496840 32 7227253 157 348445 424 7650687 760 5985040 833 859 4526177 60609 3285425 4632 43518 15994367 76798 730617 7 46250 833 159855 146 749939 19 344753 254 156537 99158 33763146 12472 668518 19 76798 8070075 9496840 32";
+            clenText.Text = "5 1 5 3 5 1 2 2 1 5 4 2 5 1 1 4 4 3 2 3 4 5 4 4 3 5 2 5 3 1 4 4 4 1 3 3 5 4 2 5 5 4 4 2 3 5 5 1 4 5 3 5 2 5 3 3 5 4 4 5 1 3 3 4 5 1 4 5 5 1 5 2 5 2 5 2 5 2 2 5 4 5 3 4 5 4 4 2 3 2 5 2 5 1 4 2 4 4 5 3 5 1 4 5 5 1";
+            ccountText.Text = "106";
+        }
+
+        private void Button_Decipher(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string output = Model.RunDecipher(cipherText.Text, clenText.Text, ccountText.Text);
+                OutputText.Text = output;
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("You haz error");
+                OutputText.Text = "Error in formatting";
+            }
+        }
+
+        private void Button_Clear(object sender, RoutedEventArgs e)
+        {
+            cipherText.Clear();
+            clenText.Clear();
+            ccountText.Clear();
+            OutputText.Clear();
+        }
+
+    }
+}
---
+++ model.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackOpsMasmCipherSolver
+{
+    static class Model
+    {
+        public static string RunDecipher(string cipher, string clen, string ccount)
+        {
+            //split string[] cipher into integer list
+            string[] cipherString = cipher.Split(' ');
+            List<UInt32> cipherInts = new List<UInt32>();
+            foreach (String ss in cipherString)
+            {
+                cipherInts.Add(UInt32.Parse(ss));
+            }
+            //split string[] clen into integer list
+            string[] clenString = clen.Split(' ');
+            List<int> clenInts = new List<int>();
+            foreach (String ss in clenString)
+            {
+                clenInts.Add(int.Parse(ss));
+            }
+            //convert ccount into an integer value
+            int ccountLoop = int.Parse(ccount);
+
+            //static data values used in cipher operations
+            UInt32 mult = Convert.ToUInt32("41c64e6d", 16);
+            UInt32 add1 = Convert.ToUInt32("3039", 16);
+            UInt32 idiv1 = Convert.ToUInt32("8000", 16);
+            UInt32 idiv2 = Convert.ToUInt32("1A", 16);
+            UInt32 add2 = Convert.ToUInt32("41", 16);
+
+            //string to append letters to
+            string output = "";
+
+            for (int i = 0; i < ccountLoop; i++)
+            {
+                for (int j = 0; j < clenInts[i]; j++)
+                {
+                    UInt32 firstData = cipherInts[i];
+                    UInt32 res1 = firstData * mult;
+                    UInt32 res2 = res1 + add1;
+                    cipherInts[i] = res2;
+                    UInt32 shr = (UInt32)(res2 / Math.Pow(2, 16));
+                    UInt32 remainder1 = shr % idiv1;
+                    UInt32 remainder2 = remainder1 % idiv2;
+                    UInt32 outp = remainder2 + add2;
+                    char ascii = (char)outp;
+                    output += ascii.ToString();
+                }
+                output += " ";
+            }
+            return output;
+        }
+    }
+}
